a94f0cfa2454bd370dfcf16b9f1c54c147fba5ea
Alex Costisor <alexcostisor@Alexs-MacBook-Air.local>
2024-11-16 11:38:00 -0500

    initial commit

--- bot.py
+++ bot.py
@@ -5,18 +5,10 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
-
-
-@bot.event
-async def welcome():
-    channel = bot.get_channel(creds.WELCOME_CHANNEL_ID)
-    await channel.send("Welcome to the server!")
-
-
 @bot.event
 async def on_ready():
     channel = bot.get_channel(creds.WELCOME_CHANNEL_ID)
-    print(f"Bot is ready in {channel}")
+    print(f"Bot is ready in #{channel}")
     await channel.send("Bot INITIALIZED!")
 
 @bot.command()
