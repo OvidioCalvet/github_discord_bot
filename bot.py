12b3407963b2119cb800a0e2f105a0af3ee43b61
Alex Costisor <alexcostisor@Alexs-MacBook-Air.local>
2024-11-16 11:12:35 -0500

    Disconnect Command

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
-import creds
+import creds 
 import discord
 from discord import Color
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
+
+
 @bot.event
 async def welcome():
     channel = bot.get_channel(creds.WELCOME_CHANNEL_ID)
-    await channel.send("test test test")
+    await channel.send("Welcome to the server!")
+
+
+@bot.event
+async def on_ready():
+    channel = bot.get_channel(creds.WELCOME_CHANNEL_ID)
+    print(f"Bot is ready in {channel}")
+    await channel.send("Bot INITIALIZED!")
+
+@bot.command()
+async def disconnect(ctx):
+    await ctx.send("Disconnecting...")
+    await ctx.send("Bot DISCONNECTED!")
+    await bot.close()
 
 bot.run(creds.BOT_TOKEN)
